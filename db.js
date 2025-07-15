f424919a5733c41393aa2617362b8a0095a36ad0
Lakshya <lakshya@cloudsciencelabs.com>
2025-07-15 13:40:11 +0530

    changes

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.PG_URI,
+   ssl: {
+    rejectUnauthorized: false, // Required for Render-managed PostgreSQL
+  },
 });
 
 export default pool;
